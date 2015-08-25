f8079ea8cce8f7efbf19d7614cba5c9b4ed79940
Laurens De Graeve <laurens.degraeve@ugent.be>
2015-08-25 11:17:01 +0200

    forgot to add new files

---
+++ app/models/functional_enrichments.php
@@ -0,0 +1,57 @@
+<?php
+  /*
+   * This model represents info on the transcripts
+   */
+class FunctionalEnrichments extends AppModel{
+
+  var $name	= 'FunctionalEnrichments';
+  var $useTable = 'functional_enrichments';
+
+  
+ function getLabeltoEnrichedInterproMapping($exp_id){
+    $result	= array();
+    $query	= "SELECT  `functional_enrichments`.label, 
+                       `functional_enrichments`.identifier,
+                       `functional_enrichments`.is_hidden,
+                       `functional_enrichments`.max_p_value,
+                       `functional_enrichments`.subset_ratio
+               FROM `functional_enrichments` 
+               WHERE experiment_id = $exp_id
+                   AND data_type = 'ipr'";    
+    $res	= $this->query($query);
+    foreach($res as $r){
+      $label	= $r['functional_enrichments']['label'];
+      $interpro	= $r['functional_enrichments']['identifier'];
+      $hidden	= $r['functional_enrichments']['is_hidden'];
+      $p_val	= $r['functional_enrichments']['max_p_value'];
+      $ratio	= $r['functional_enrichments']['subset_ratio'];
+      $result[] = array($label,$interpro,$ratio,$hidden,$p_val);
+    }    
+    return $result;
+  }
+
+ function getLabeltoEnrichedGOMapping($exp_id){
+    $result	= array();
+    $query	= "SELECT `functional_enrichments`.label, 
+                      `functional_enrichments`.identifier,
+                      `functional_enrichments`.is_hidden,
+                      `functional_enrichments`.max_p_value,
+                      `functional_enrichments`.subset_ratio
+               FROM `functional_enrichments` 
+               WHERE experiment_id = $exp_id
+                   AND data_type = 'go'";
+    $res	= $this->query($query);
+    foreach($res as $r){
+      $label	= $r['functional_enrichments']['label'];
+      $GO	    = $r['functional_enrichments']['identifier'];
+      $hidden	= $r['functional_enrichments']['is_hidden'];
+      $p_val	= $r['functional_enrichments']['max_p_value'];
+      $ratio	= $r['functional_enrichments']['subset_ratio'];
+      $result[] = array($label,$GO,$ratio,$hidden,$p_val);
+    }    
+    return $result;
+  }
+
+}
+
+?>
