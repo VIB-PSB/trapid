3b28615bbb49140ba8d817dad1b5baec300c3131
François Bucchini <frbuc@psb.vib-ugent.be>
2017-12-20 22:06:32 +0100

    Add core GF completeness results model

---
+++ app/Model/CompletenessResults.php
@@ -0,0 +1,9 @@
+<?php
+
+/* Model to deal with core GF completeness analysis results */
+
+class CompletenessResults extends AppModel {
+    var $name	= 'CompletenessResults';
+    var $useTable	= 'completeness_results';
+
+}
