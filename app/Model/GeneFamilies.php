c9ee5dee4896933eabb12013ef1c5ea7d27a681e
François Bucchini <frbuc@psb.vib-ugent.be>
2022-06-28 18:36:45 +0200

    Cleanup GeneFamilies model

--- app/Model/GeneFamilies.php
+++ app/Model/GeneFamilies.php
@@ -1,64 +1,45 @@
 <?php
-  /*
-   * This model represents info on the transcripts
-   */
+
+/*
+ * This model represents gene family information associated with an experiment's transcripts.
+ */
+
 class GeneFamilies extends AppModel {
 
-  var $name	= 'GeneFamilies';
-  var $useTable = 'gene_families';
-
-
-
-
-  function findByGene($exp_id,$gene_id){
-    $data_source = $this->getDataSource();
-    $exp_id	= $data_source->value($exp_id, 'integer');
-    $result	= array();
-    if(strlen($gene_id) <= 5){return $result;}
-
-    $gf_data = $this->find("all",
-        array("conditions"=>array("experiment_id"=>$exp_id, "gf_content LIKE"=>"%$gene_id%"),
-              "fields"=>array("gf_id", "plaza_gf_id", "num_transcripts")));
-    // $query	= "SELECT `gf_id`,`plaza_gf_id`,`num_transcripts` FROM `gene_families` WHERE `experiment_id`='".$exp_id."' AND `gf_content` LIKE '%".$gene_id."%' ";
-    // pr($query)
-    // $res	= $this->query($query);
-    // foreach($res as $r){
-    //    $result = $r['gene_families'];
-    foreach($gf_data as $r){
-      $result = $r['GeneFamilies'];
-    }
-    return $result;
-
-  }
-
-
-  function getGfInfo($exp_id,$gf_id){
-    $data_source = $this->getDataSource();
-    $exp_id	= $data_source->value($exp_id, 'integer');
-    $gf_id	= $data_source->value($gf_id, 'string');
-
-    $query 	= "SELECT a.*,b.`transcript_id` FROM `gene_families` a,`transcripts` b 
-			WHERE a.`experiment_id`='".$exp_id."' AND b.`experiment_id`='".$exp_id."' 
-			AND a.`gf_id`=".$gf_id." AND b.`gf_id`=a.`gf_id` ";
-    $res	= $this->query($query);
-    pr($res);
-
-    return 12;
-  }
-
-
-    function gfExists($exp_id, $gf_id){
-
+    function findByGene($exp_id, $gene_id) {
         $data_source = $this->getDataSource();
-        $exp_id	= $data_source->value($exp_id, 'integer');
-        $gf_id	= $data_source->value($gf_id, 'string');
-        $query = "SELECT * FROM `gene_families` WHERE `experiment_id`='".$exp_id."' AND `gf_id`=".$gf_id.";";
-        $res = $this->query($query);
-        if($res)
-            return true;
-        else
-            return false;
+        $exp_id = $data_source->value($exp_id, 'integer');
+        $result = array();
+        if (strlen($gene_id) <= 5) {
+            return $result;
+        }
+
+        $gf_data = $this->find("all",
+            array("conditions" => array("experiment_id" => $exp_id, "gf_content LIKE" => "%$gene_id%"),
+                "fields" => array("gf_id", "plaza_gf_id", "num_transcripts")));
+        // $query  = "SELECT `gf_id`,`plaza_gf_id`,`num_transcripts` FROM `gene_families` WHERE `experiment_id`='".$exp_id."' AND `gf_content` LIKE '%".$gene_id."%' ";
+        // $res    = $this->query($query);
+        // foreach($res as $r) {
+        //    $result = $r['gene_families'];
+        // }
+        foreach ($gf_data as $r) {
+            $result = $r['GeneFamilies'];
+        }
+        return $result;
     }
 
+    function gfExists($exp_id, $gf_id) {
+        $data_source = $this->getDataSource();
+        $exp_id = $data_source->value($exp_id, 'integer');
+        $gf_id = $data_source->value($gf_id, 'string');
+        $query = "SELECT * FROM `gene_families` WHERE `experiment_id`='" . $exp_id . "' AND `gf_id`=" . $gf_id . ";";
+        $res = $this->query($query);
+        if ($res) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
 }
