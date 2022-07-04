3d322a0a2f8f123adcecf9c76945872cd8475216
François Bucchini <frbuc@psb.vib-ugent.be>
2022-07-04 21:49:19 +0200

    Cleanup models

--- app/Model/CompletenessResults.php
+++ app/Model/CompletenessResults.php
@@ -3,7 +3,5 @@
 /* Model to deal with core GF completeness analysis results */
 
 class CompletenessResults extends AppModel {
-    var $name	= 'CompletenessResults';
-    var $useTable	= 'completeness_results';
 
 }
