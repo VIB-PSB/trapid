ef4f5c6b67341bb2f6ed769826ad18e59b7dcc66
François Bucchini <frbuc@psb.vib-ugent.be>
2022-08-21 11:38:26 +0200

    Reformat controller

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -8,17 +8,16 @@ App::uses('CakeEmail', 'Network/Email');
  */
 
 class AppController extends Controller {
-
-    var $components = array("Cookie");
-    var $helpers = array("Html", "Form");
-    var $process_states = array("default" => array("empty", "upload", "finished"),
-        "all" => array("empty", "upload", "finished", "processing", "error"),
-        "finished" => array("finished"),
-        "upload" => array("upload"),
-        "start" => array("empty", "upload")
-    );
-    var $uses = array("Authentication", "Experiments", "SharedExperiments");
-
+    var $components = ['Cookie'];
+    var $helpers = ['Html', 'Form'];
+    var $process_states = [
+        'default' => ['empty', 'upload', 'finished'],
+        'all' => ['empty', 'upload', 'finished', 'processing', 'error'],
+        'finished' => ['finished'],
+        'upload' => ['upload'],
+        'start' => ['empty', 'upload']
+    ];
+    var $uses = ['Authentication', 'Experiments', 'SharedExperiments'];
 
     /*
      * Cookie setup:
@@ -33,7 +32,7 @@ class AppController extends Controller {
      */
     function beforeFilter() {
         parent::beforeFilter();
-        $this->set("title", WEBSITE_TITLE);
+        $this->set('title', WEBSITE_TITLE);
         $this->Cookie->name = COOKIE_NAME;
         $this->Cookie->time = COOKIE_TIME;
         $this->Cookie->domain = COOKIE_DOMAIN;
@@ -42,146 +41,139 @@ class AppController extends Controller {
         $this->Cookie->secure = COOKIE_SECURE;
     }
 
-
     /*
      * Authentication of user-experiment combination,
      * not through cookie, but through extra hashed variables.
      */
     function check_user_exp_no_cookie($hashed_user_id = null, $exp_id = null) {
-        //get experiment info
+        // Get experiment info
         if (!$hashed_user_id || !$exp_id) {
             return false;
         }
 
-        //experiment must exist!
-        $exp_info = $this->Experiments->find("first", array("conditions" => array("experiment_id" => $exp_id)));
+        // Check experiment exists
+        $exp_info = $this->Experiments->find('first', ['conditions' => ['experiment_id' => $exp_id]]);
         if (!$exp_info) {
             return false;
         }
 
-        //check admin hashes
-        $admin_users = $this->Authentication->find("all", array(
-            "conditions" => array("group" => "admin"), "fields" => "user_id")
-        );
+        // Check admin hashes
+        $admin_users = $this->Authentication->find('all', [
+            'conditions' => ['group' => 'admin'],
+            'fields' => 'user_id'
+        ]);
         foreach ($admin_users as $au) {
-            $t = $au["Authentication"]["user_id"] . $this->Cookie->key;
-            $new_hashed_user_id = hash("md5", $t);
+            $t = $au['Authentication']['user_id'] . $this->Cookie->key;
+            $new_hashed_user_id = hash('md5', $t);
             if ($new_hashed_user_id == $hashed_user_id) {
                 return true;
             }
         }
 
-        //check the owner of the experiment itself
+        // Check the owner of the experiment itself
         $user_id = $exp_info['Experiments']['user_id'];
-        $t = $user_id . "" . $this->Cookie->key;
-        $new_hashed_user_id = hash("md5", $t);
+        $t = $user_id . '' . $this->Cookie->key;
+        $new_hashed_user_id = hash('md5', $t);
         if ($hashed_user_id == $new_hashed_user_id) {
             return true;
         }
 
-        //ok, no admin and owner, but perhaps a shared experiment?
-        $shared_users = $this->SharedExperiments->find("all", array(
-            "conditions" => array("experiment_id" => $exp_id), "fields" => "user_id")
-        );
+        // Shared experiment: check if user can access it
+        $shared_users = $this->SharedExperiments->find('all', [
+            'conditions' => ['experiment_id' => $exp_id],
+            'fields' => 'user_id'
+        ]);
         foreach ($shared_users as $su) {
             $t = $su['SharedExperiments']['user_id'] . $this->Cookie->key;
-            $new_hashed_user_id = hash("md5", $t);
+            $new_hashed_user_id = hash('md5', $t);
             if ($new_hashed_user_id == $hashed_user_id) {
                 return true;
             }
         }
-
         return false;
     }
 
-
     function get_hashed_user_id() {
-        $user_id = $this->Cookie->read("user_id");
-        $t = $user_id . "" . $this->Cookie->key;
-        $hashed_user_id = hash("md5", $t);
+        $user_id = $this->Cookie->read('user_id');
+        $t = $user_id . '' . $this->Cookie->key;
+        $hashed_user_id = hash('md5', $t);
         return $hashed_user_id;
     }
-
 
     /**
      * Authentication of user through cookie-data
      */
     function check_user() {
-        $user_id = $this->Cookie->read("user_id");
-        $email = $this->Cookie->read("email");
-        // No need to escape SQL data when using `find` and proper array notation?
-        // See: https://stackoverflow.com/questions/3534243/how-do-you-escape-sql-data-in-cakephp
-        $user_data = $this->Authentication->find("first", array(
-            "conditions" => array("user_id" => $user_id, "email" => $email)
-        ));
+        $user_id = $this->Cookie->read('user_id');
+        $email = $this->Cookie->read('email');
+        $user_data = $this->Authentication->find('first', [
+            'conditions' => ['user_id' => $user_id, 'email' => $email]
+        ]);
         if (!$user_data) {
-            $this->redirect(array("controller" => "trapid", "action" => "authentication"));
+            $this->redirect(['controller' => 'trapid', 'action' => 'authentication']);
         }
-        return $user_data["Authentication"]["user_id"];
+        return $user_data['Authentication']['user_id'];
     }
-
 
     function is_owner($exp_id = null) {
         if (!$exp_id) {
-            $this->redirect(array("controller" => "trapid", "action" => "experiments"));
+            $this->redirect(['controller' => 'trapid', 'action' => 'experiments']);
         }
         $user_id = $this->check_user();
-        $user_data = $this->Authentication->find("first", array("conditions" => array("user_id" => $user_id)));
-        $experiment = $this->Experiments->find("first", array("conditions" => array("experiment_id" => $exp_id)));
-        //possibility 1: the experiment is private, the user is not an admin, and the user matches the experiment.
+        $user_data = $this->Authentication->find('first', ['conditions' => ['user_id' => $user_id]]);
+        $experiment = $this->Experiments->find('first', ['conditions' => ['experiment_id' => $exp_id]]);
+        // Possibility 1: the experiment is private, the user is not an admin, and the user matches the experiment.
         if ($experiment['Experiments']['user_id'] == $user_id) {
             return true;
         }
-        //possibility 3: the experiment is private, the user is an admin
-        else if ($user_data['Authentication']['group'] == "admin") {
+        // Possibility 2: the experiment is private but the user is an admin
+        elseif ($user_data['Authentication']['group'] == 'admin') {
             return true;
         }
         return false;
     }
-
 
     /*
      * Authentication of user and experiment
      */
     function check_user_exp($exp_id = null) {
         if (!$exp_id) {
-            $this->redirect(array("controller" => "trapid", "action" => "experiments"));
+            $this->redirect(['controller' => 'trapid', 'action' => 'experiments']);
         }
         $user_id = $this->check_user();
-        $user_data = $this->Authentication->find("first", array("conditions" => array("user_id" => $user_id)));
-        $shared_exp = $this->SharedExperiments->find("first", array(
-            "conditions" => array("user_id" => $user_id, "experiment_id" => $exp_id))
-        );
+        $user_data = $this->Authentication->find('first', ['conditions' => ['user_id' => $user_id]]);
+        $shared_exp = $this->SharedExperiments->find('first', [
+            'conditions' => ['user_id' => $user_id, 'experiment_id' => $exp_id]
+        ]);
 
-        $experiment = $this->Experiments->find("first", array("conditions" => array("experiment_id" => $exp_id)));
+        $experiment = $this->Experiments->find('first', ['conditions' => ['experiment_id' => $exp_id]]);
 
         // Possibility 1: the experiment is a public experiment. And as such visible to everyone.
         if ($experiment['Experiments']['public_experiment'] == 1) {
-            $this->changeDbConfigs($exp_id);    //no need to check on user-experiment
+            $this->changeDbConfigs($exp_id); //no need to check on user-experiment
             return;
         }
         // Possibility 2: the experiment is private, the user is not an admin, and the user matches the experiment.
-        else if ($experiment['Experiments']['user_id'] == $user_id) {
+        elseif ($experiment['Experiments']['user_id'] == $user_id) {
             $this->changeDbConfigs($exp_id);
             return;
         }
         // Possibility 3: the experiment is private, the user is an admin
-        else if ($user_data['Authentication']['group'] == "admin") {
+        elseif ($user_data['Authentication']['group'] == 'admin') {
             $this->changeDbConfigs($exp_id);
             return;
         }
         // Possibility 4: experiment is a shared experiment between users.
-        else if ($shared_exp) {
+        elseif ($shared_exp) {
             $this->changeDbConfigs($exp_id);
             return;
         }
         // Possibility 5: the experiment is private, the user is not an admin, and the user does not match the experiment
         // In that case, redirect to the `experiments` page.
         else {
-            $this->redirect(array("controller" => "trapid", "action" => "experiments"));
+            $this->redirect(['controller' => 'trapid', 'action' => 'experiments']);
         }
     }
-
 
     /*
      * Important part of the communication with non-trapid databases. These databases could be anything, and as such we
@@ -189,9 +181,10 @@ class AppController extends Controller {
      */
     function changeDbConfigs($exp_id) {
         // Get the experiment's reference database (historically 'plaza' database, hence the field name).
-        $exp_ref_db = $this->Experiments->find("first", array(
-            "conditions" => array("experiment_id" => $exp_id), "fields" => "used_plaza_database")
-        );
+        $exp_ref_db = $this->Experiments->find('first', [
+            'conditions' => ['experiment_id' => $exp_id],
+            'fields' => 'used_plaza_database'
+        ]);
         $exp_ref_db = $exp_ref_db['Experiments']['used_plaza_database'];
         // Set new `useDBConfig` for models
         $this->AnnotSources->useDbConfig = $exp_ref_db;
@@ -202,5 +195,4 @@ class AppController extends Controller {
         $this->GfData->useDbConfig = $exp_ref_db;
         $this->KoTerms->useDbConfig = $exp_ref_db;
     }
-
 }
